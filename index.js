9e93a2d6d467e6d727906b7f5fa15ccc8618673d
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-11-21 14:19:51 -0500

    chore: change for my google sheet and remove ejs

--- index.js
+++ index.js
@@ -2,54 +2,38 @@ const express = require("express");
 const { google } = require("googleapis");
 
 const app = express();
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.post("/", async (req, res) => {
-  const { request, name } = req.body;
+app.post("/v1/write-sheet", async (req, res) => {
+  const { name, lastName, phonenumber, birthday } = req.body;
 
   const auth = new google.auth.GoogleAuth({
     keyFile: "credentials.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
 
-  // Create client instance for auth
   const client = await auth.getClient();
-
-  // Instance of Google Sheets API
   const googleSheets = google.sheets({ version: "v4", auth: client });
-
-  const spreadsheetId = "1J5OesnSTJCgLTTA0hQ_QSk_UPVK1nwRTEkvvRHHrEqM";
-
-  // Get metadata about spreadsheet
-  const metaData = await googleSheets.spreadsheets.get({
-    auth,
-    spreadsheetId,
-  });
-
-  // Read rows from spreadsheet
-  const getRows = await googleSheets.spreadsheets.values.get({
-    auth,
-    spreadsheetId,
-    range: "Sheet1!A:A",
-  });
-
+  const spreadsheetId = "1dJ3dEhx3SDTsmfwpE7eVUE52zVJ0b1rDPWFH2KI5VoY";
+  
   // Write row(s) to spreadsheet
-  await googleSheets.spreadsheets.values.append({
+  googleSheets.spreadsheets.values.append({
     auth,
     spreadsheetId,
-    range: "Sheet1!A:B",
+    range: "recopilacion!A:D",
     valueInputOption: "USER_ENTERED",
     resource: {
-      values: [[request, name]],
+      values: [
+        [ name, lastName, phonenumber, birthday ],
+      ],
     },
+  }, (err, info) => {
+    if (err) {
+      res.status(500).json({ ok: false, msg: err.toString() });
+    } else {
+      res.status(200).json({ ok: true, msg: "Se escribio correctamente" });
+    }
   });
-
-  res.send("Successfully submitted! Thank you!");
 });
 
-app.listen(1337, (req, res) => console.log("running on 1337"));
+app.listen(1337, () => console.log("running on 1337"));
